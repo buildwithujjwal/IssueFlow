60fd7f10f3bbd5c139c96e0aed637b0a68c670fe
buildwithujjwal <ujjwal.work04@gmail.com>
2026-06-26 18:46:30 +0530

    feat: add search to issue by title and also add filtering to issue by status and priority

--- src/controllers/issueController.js
+++ src/controllers/issueController.js
@@ -15,7 +15,15 @@ const createIssue = async (req, res) => {
 
 const getIssuesByProject = async (req, res) => {
     try{
-        const issues = await Issue.find({project: req.params.id})
+
+        // filtering by which project, status, priority and by search also
+        const filter = {project: req.params.id}
+        if(req.query.status) filter.status = req.query.status
+        if(req.query.priority) filter.priority = req.query.priority
+        if(req.query.search) filter.title = { $regex: req.query.search, $options: "i" }
+
+        // finding issues according to filter
+        const issues = await Issue.find(filter)
         if(issues.length == 0) return res.status(404).json({message: 'No Issue Found'})
         return res.status(200).json(issues)
     }
